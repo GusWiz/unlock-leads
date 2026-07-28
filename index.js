ee3527ba2840a8ef46cf9efbc17eb8ccb3e6f0e1
Gustavo <pow10@txstate.edu>
2026-07-28 15:33:23 -0500

    changed html elements to be set as const.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el") 
+myInputs = []
 
 inputBtn.addEventListener("click", function() {
     console.log("Saving input")
