d557a91ca0517a3425062e0d80c9b48d48c35854
Gustavo <pow10@txstate.edu>
2026-07-30 12:16:16 -0500

    refactor: clear myLeads array and update renderLead function

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el") 
-myLeads = ["awesomeLeans", "www1241", "werwer141"]
+myLeads = []
 
 ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLead()
+    inputEl.value = ''
+
 })
 
-let listItems = ''
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+function renderLead() {
+    // let listItem = "<li>" + myLeads[myLeads.length - 1] + "</li>"
+    let listItem = "<li>" + inputEl.value + "</li>"
+    ulEl.innerHTML += listItem
 }
-
-ulEl.innerHTML= listItems
