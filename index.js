1de8ae39c131e5001f1637fa9486b97bfbf2d6d2
Gustavo <pow10@txstate.edu>
2026-08-03 14:43:26 -0500

    refactor: update renderLeads function to improve link rendering and local storage handling

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el") 
-myLeads = []
-
 ulEl = document.getElementById("ul-el")
+
+leadsFromLocalStorage = localStorage.getItem("myLeads")
+
+if (leadsFromLocalStorage){ 
+    myLeads = JSON.parse(leadsFromLocalStorage)
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    renderLead()
     inputEl.value = ''
-
+    // saving myLeads (array) to local storage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    // console.log(localStorage.getItem("myLeads"))
+    renderLeads()
 })
 
-function renderLead() {
+function renderLeads() {
     // let listItem = "<li>" + myLeads[myLeads.length - 1] + "</li>"
-    let listItem = `
-    <li>
-        <a target='_blank' href='${inputEl.value}'>
-            ${inputEl.value}
-        </a>
-    </li>
-    `
-    ulEl.innerHTML += listItem
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
