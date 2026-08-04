c5b56f14c313c4d170b1b18472d373850a3dcde8
Gustavo <pow10@txstate.edu>
2026-08-04 16:12:24 -0500

    refactor: consolidate render functionality and improve code readability

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el") 
 const deleteBtn = document.getElementById("delete-btn")
-ulEl = document.getElementById("ul-el")
-
-
-leadsFromLocalStorage = localStorage.getItem("myLeads")
+const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = localStorage.getItem("myLeads")
 
 if (leadsFromLocalStorage){ 
     myLeads = JSON.parse(leadsFromLocalStorage)
-    renderLeads()
+    render(myLeads)
+}
+
+function render(leads) {
+    // let listItem = "<li>" + myLeads[myLeads.length - 1] + "</li>"
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
 
 inputBtn.addEventListener("click", function() {
@@ -18,25 +31,11 @@ inputBtn.addEventListener("click", function() {
     // saving myLeads (array) to local storage
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     // console.log(localStorage.getItem("myLeads"))
-    renderLeads()
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
-function renderLeads() {
-    // let listItem = "<li>" + myLeads[myLeads.length - 1] + "</li>"
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
-            </a>
-        </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
